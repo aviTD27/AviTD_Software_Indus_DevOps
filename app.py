b1030990d4f5bb2580748d99b9cfd25583922b51
aviTD27 <avi.doorga27@gmail.com>
2026-06-10 21:00:41 +0200

    Feat: Update to add different versions

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 INSTANCE_NAME = os.environ.get("INSTANCE_NAME", "unknown")
+VERSION = os.environ.get("VERSION", "unknown")
 
 @app.route("/version")
 def version():
     return jsonify({
         "instance": INSTANCE_NAME,
-        "version": "2.0"
+        "version": VERSION
     })
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
